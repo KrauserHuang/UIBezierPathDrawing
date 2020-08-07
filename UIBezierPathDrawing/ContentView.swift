8501e6fe76455fa823fd127bc13c2487bd9b1ab7
krauserhuang <crescentdence@gmail.com>
2020-08-07 12:03:58 +0800

    Initial Commit

---
+++ UIBezierPathDrawing/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  UIBezierPathDrawing
+//
+//  Created by Tai Chin Huang on 2020/8/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
